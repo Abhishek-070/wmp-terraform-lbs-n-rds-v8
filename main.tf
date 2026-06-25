00d5205ccf6dbac39d5163bb325b5ae98136f995
abhishekdontukurti <dabhishek175@gmail.com>
2026-06-25 18:54:20 -0400

    learn-k8s

--- main.tf
+++ main.tf
@@ -8,22 +8,22 @@ module "databases" {
   allocated_storage = each.value["allocated_storage"]
 }
 
-module "apps" {
-  depends_on = [module.databases]
-  source     = "./modules/components-with-alb"
-
-  dns_domain = var.dns_domain
-  env        = var.env
-  subnets    = var.subnets
-  vpc_id     = var.vpc_id
-
-  for_each      = var.apps
-  component     = each.key
-  instance_type = each.value["instance_type"]
-  ports         = each.value["ports"]
-  lb            = each.value["lb"]
-  asg           = each.value["asg"]
-
-  postgres_rds_address = module.databases["postgresql"].postgres_rds_address
+# module "apps" {
+#   depends_on = [module.databases]
+#   source     = "./modules/components-with-alb"
+#
+#   dns_domain = var.dns_domain
+#   env        = var.env
+#   subnets    = var.subnets
+#   vpc_id     = var.vpc_id
+#
+#   for_each      = var.apps
+#   component     = each.key
+#   instance_type = each.value["instance_type"]
+#   ports         = each.value["ports"]
+#   lb            = each.value["lb"]
+#   asg           = each.value["asg"]
+#
+#   postgres_rds_address = module.databases["postgresql"].postgres_rds_address
 
 }
